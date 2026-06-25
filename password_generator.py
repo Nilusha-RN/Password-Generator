ca17f8ec8270d65664c09e4057ccc5b6ba2dcb5c
Nilusha-RN <nilushanirmal883@gmail.com>
2026-06-25 07:40:14 +0530

    Ask user for password length

--- password_generator.py
+++ password_generator.py
@@ -3,11 +3,13 @@ import string
 
 print("=== Password Generator ===")
 
+length = int(input("Enter your password length: "))
+
 characters = string.ascii_letters + string.digits + string.punctuation
 
 password = ""
 
-for i in range(12):
+for i in range(length):
     password += random.choice(characters)
 
 print("Generated Password:", password)
