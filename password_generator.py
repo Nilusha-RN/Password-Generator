450242c92c0c87e5b3e50dada64469c9b67cdc3a
Nilusha-RN <nilushanirmal883@gmail.com>
2026-06-28 06:45:26 +0530

    Add character type selection

--- password_generator.py
+++ password_generator.py
@@ -11,18 +11,34 @@ try:
 
     else:
         use_uppercase = input("Include uppercase letters? (yes/no): ").lower()
+        use_lowercase = input("Include lowercase letters? (yes/no): ").lower()
+        use_numbers = input("Include numbers? (yes/no): ").lower()
+        use_symbols = input("Include symbols? (yes/no): ").lower()
 
-        characters = string.ascii_lowercase + string.digits + string.punctuation
+        characters = ""
 
         if use_uppercase == "yes":
             characters += string.ascii_uppercase
 
-        password = ""
+        if use_lowercase == "yes":
+            characters += string.ascii_lowercase
 
-        for i in range(length):
-            password += random.choice(characters)
+        if use_numbers == "yes":
+            characters += string.digits
 
-        print("Generated Password:", password)
+        if use_symbols == "yes":
+            characters += string.punctuation
+
+        if characters == "":
+            print("You must select at least one character type.")
+
+        else:
+            password = ""
+
+            for i in range(length):
+                password += random.choice(characters)
+
+            print("Generated Password:", password)
 
 except ValueError:
     print("Please enter a valid number.")
